07344194ca90a61710a1a0a1157b35d297f6bcef
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-01-29 22:51:30 +0530

    Add Reset Function

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App(){
+function App() {
+  const [count, setCount] = useState(0); // useState is a hook. used to show the state of the component.
 
-  const [count, setCount] = useState(0);
-
-  const decreaseHandler = ()=>{
-    setCount(count-1);
+  const decreaseHandler = () => {
+    setCount(count - 1);
     // console.log(count);
   };
 
-  const increaseHandler = ()=>{
-    setCount(count+1);
+  const increaseHandler = () => {
+    setCount(count + 1);
     // console.log(count);
   };
 
-  const resetHandler =()=>{
+  const resetHandler = () => {
     setCount(0);
-  }
+  };
 
   return (
-    <div className='w-[100vw] h-[100vh] flex flex-col justify-center items-center bg-[#344151] gap-10'>
-      <div className='text-[#0398d4] font-medium text-2xl'>Increment & Decrement</div>
-      <div className='bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]'>
-        <button onClick={decreaseHandler} className='border-r-2 text-center w-20 border-[#bfbfbf] text-5xl'>
+    <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center bg-[#344151] gap-10">
+      <div className="text-[#0398d4] font-medium text-2xl">
+        Increment & Decrement
+      </div>
+      <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
+        <button
+          onClick={decreaseHandler}
+          className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl"
+        >
           -
         </button>
-        <div className='font-bold gap-12 text-5xl'>
-          {count}
-        </div>
-        <button onClick={increaseHandler} className='border-l-2 text-center w-20 border-[#bfbfbf] text-5xl'>
+        <div className="font-bold gap-12 text-5xl">{count}</div>
+        <button
+          onClick={increaseHandler}
+          className="border-l-2 text-center w-20 border-[#bfbfbf] text-5xl"
+        >
           +
         </button>
       </div>
-      <button onClick={resetHandler} className='bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg'>
-        Reset</button>
+      <button
+        onClick={resetHandler}
+        className="bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg"
+      >
+        Reset
+      </button>
     </div>
   );
 }
